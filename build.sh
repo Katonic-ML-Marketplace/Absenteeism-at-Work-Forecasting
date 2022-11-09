8d2f3c227b2b3e40db2c0473a41ab7d8358e9038
shreya01807 <disha.uke@katonic.ai>
2022-11-09 10:19:15 +0000

    added files

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t katonic/apps:absenteeism-at-work-forecast .
+docker push katonic/apps:absenteeism-at-work-forecast
+# docker run --rm -p 8050:8050 katonic/apps:absenteeism-at-work-forecast
